7e27fcad1d2df68024f8d834739063e6161eee3a
Falcão <gabriel.p.falcao50@gmail.com>
2025-03-09 21:56:33 -0400

    feat: novos comandos botinfo e invite

---
+++ src/commands/botinfo.js
@@ -0,0 +1,29 @@
+const { msToTime } = require("../utils/functions")
+const { SlashCommandBuilder } = require("discord.js")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("botinfo")
+		.setDescription("Check some bot stats")
+		.setDescriptionLocalizations({
+			"pt-BR": "Veja algumas estatísticas do bot",
+			"es-ES": "Mira algunas estadísticas del bot",
+		})
+		.setDMPermission(false),
+	execute: async ({ client, interaction, instance }) => {
+		await interaction.deferReply().catch(() => {})
+		try {
+			const embed = instance.createEmbed(3426654).addFields({
+				name: "Falforces",
+				value: `:house: Servidores: ${client.guilds.cache.size}\n:zap: Online por: *${msToTime(client.uptime)}*`,
+			})
+			await instance.editReply(interaction, { embeds: [embed] })
+		} catch (error) {
+			console.error(`botinfo: ${error}`)
+			instance.editReply(interaction, {
+				content: instance.getMessage(interaction, "EXCEPTION"),
+				embeds: [],
+			})
+		}
+	},
+}
